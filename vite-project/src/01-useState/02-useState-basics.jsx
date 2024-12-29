e2470752ab9510e717e8e4ad93ec03bede46b01c
saquibe <saquibe143@gmail.com>
2024-12-29 19:21:58 +0530

    update count using useState

---
+++ vite-project/src/01-useState/02-useState-basics.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const UseStateBasics = () => {
+  // console.log(useState(0));
+  // console.log(useState("Hello World"));
+  // const value = useState("hello")[0];
+  // const func = useState("hello")[1];
+
+  const [count, setCount] = useState(0);
+
+  const hadleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <h4>{`You clicked ${count} times.`}</h4>
+      <button className="btn" onClick={hadleClick}>
+        Click me
+      </button>
+    </div>
+  );
+};
+
+export default UseStateBasics;
--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import ErrorExample from "./01-useState/01-error-example";
+import UseStateBasics from "./01-useState/02-useState-basics";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <ErrorExample />
-    </>
+      <UseStateBasics />
+    </div>
   );
 }
 
