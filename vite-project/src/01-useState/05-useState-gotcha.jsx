e1e1c6c3ba59855d92c962af046b00b78b4a57b3
saquibe <saquibe143@gmail.com>
2024-12-31 12:09:13 +0530

    update UseStateGotcha to delay state update with setTimeout

--- vite-project/src/01-useState/05-useState-gotcha.jsx
+++ vite-project/src/01-useState/05-useState-gotcha.jsx
@@ -4,17 +4,18 @@ function UseStateGotcha() {
   const [value, setValue] = useState(0);
 
   const handleClick = () => {
-    // setValue(value + 1);
-    // console.log(value);
-    setValue((currentState) => {
-      const newState = currentState + 1;
-      return newState;
-    });
+    setTimeout(() => {
+      // console.log("i clicked on the button");
+      // setValue(value + 1);
+      setValue((currentState) => {
+        return currentState + 1;
+      });
+    }, 3000);
   };
   return (
     <div>
       <h2>{value}</h2>
-      <button className="btn" onClick={handleClick}>
+      <button type="button" className="btn" onClick={handleClick}>
         Increase
       </button>
     </div>
