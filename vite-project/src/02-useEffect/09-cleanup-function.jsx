2c3b64b20f57d8a17bf8e1ecdbe0c808ce5e3a8a
saquibe <saquibe143@gmail.com>
2025-01-01 17:18:52 +0530

    cleanup function addEventListener example

--- vite-project/src/02-useEffect/09-cleanup-function.jsx
+++ vite-project/src/02-useEffect/09-cleanup-function.jsx
@@ -15,13 +15,20 @@ function CleanUpFunction() {
 
 const RandomComponent = () => {
   useEffect(() => {
-    // console.log("hmmm this is interesting");
-    const intId = setInterval(() => {
-      // console.log("hello from interval");
-    }, 1000);
+    // // console.log("hmmm this is interesting");
+    // const intId = setInterval(() => {
+    //   // console.log("hello from interval");
+    // }, 1000);
+    // return () => {
+    //   clearInterval(intId);
+    //   console.log("clean up function");
+    // };
+    const someFunction = () => {
+      console.log("some function");
+    };
+    window.addEventListener("scroll", someFunction);
     return () => {
-      clearInterval(intId);
-      console.log("clean up function");
+      window.removeEventListener("scroll", someFunction);
     };
   }, []);
   return <h2>hello there,</h2>;
