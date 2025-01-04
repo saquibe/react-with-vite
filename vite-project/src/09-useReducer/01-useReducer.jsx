44643e78f2a2c04665396c66e3e41aa751eccb3c
saquibe <saquibe143@gmail.com>
2025-01-04 18:58:35 +0530

    useReducer hook light version of redux one of the most famous state management library -  setup

--- vite-project/src/09-useReducer/01-useReducer.jsx
+++ vite-project/src/09-useReducer/01-useReducer.jsx
@@ -1,25 +1,35 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { data } from "../data";
+import { useReducer } from "react";
+
+const defaultState = {
+  people: data,
+};
+
+const reducer = () => {};
 
 const ReducerBasics = () => {
-  const [people, setPeople] = useState(data);
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  // const [people, setPeople] = useState(data);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    // let newPeople = people.filter((person) => person.id !== id);
+    // setPeople(newPeople);
   };
 
   const clearList = () => {
-    setPeople([]);
+    // setPeople([]);
   };
 
   const resetList = () => {
-    setPeople(data);
+    // setPeople(data);
   };
+  console.log(state);
 
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div key={id} className="item">
@@ -28,7 +38,7 @@ const ReducerBasics = () => {
           </div>
         );
       })}
-      {people.length < 1 ? (
+      {state.people.length < 1 ? (
         <button className="btn" onClick={resetList}>
           Reset
         </button>
